984641687681f3aff79129dadd645e0f0d237d81
gracklecracklepop <171630312+gracklecracklepop@users.noreply.github.com>
2026-09-18 08:22:46 -0700

    problem 4

---
+++ po4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+
+    for (int a = 1; a < 13; a++)
+    {
+        for (int b = 1; b < 13; b++)
+        {
+            std::cout << a << " times " << b << " is " << a * b << std::endl;
+    
+        }
+    }
+    
+ 
+
+
+ 
+
+    return 0;
+}
