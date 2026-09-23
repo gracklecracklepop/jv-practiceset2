1c7f914446d5c0a52c9a608bfb856594947dffb9
gracklecracklepop <171630312+gracklecracklepop@users.noreply.github.com>
2026-09-22 17:26:48 -0700

    problem 7

--- po7.cpp
+++ po7.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 
 
-int gcdIterative(int a, int b) {
+
+
+int main() {
+    
+    int a;
+    std::cout << "enter the first number: ";
+    std::cin >> a;
+
+    int b;
+    std::cout << "enter the second number: ";
+    std::cin >> b;
+    
     while (b != 0) {
         int remainder = a % b;
         a = b;
         b = remainder;
     }
-    return a;
-}
-
-int main() {
-     int a;
-    std::cout << "Enter the first number: ";
-    std::cin >> a;
-
-    int b;
-    std::cout << "Enter the second number: ";
-    std::cin >> b;
     
-    std::cout << "GCD of " << a << " and " << b << " is: " 
-              << gcdIterative(a, b) << std::endl; // Outputs 13
+    std::cout << "GCD is " << a << std::endl; 
               
     return 0;
 }
